9e61768de0e253ac7b37b7835ea8ec2adcc93b36
Chase Gruber <chasegruber@gmail.com>
2016-01-09 15:15:06 -0500

    working twitter oauth

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var env = require("./env");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
@@ -16,27 +17,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: env.sessionSecret }));
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set('view engine', 'hbs');
 app.set("views","./views");
-
-app.use(router)
-
-app.use(session({ secret: 'fruitbat' }));
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   next();
 });
 
-//working middleware for routes starting with /profile
-// app.all('/chat*', checkUser);
-// function checkUser(req, res, next) {
-//   if (req.isAuthenticated()) return next();
-//   res.redirect('/login');
-// }
+// working middleware for routes starting with /profile
+app.all('/users*', checkUser);
+function checkUser(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
+app.use(router)
 
 app.listen(3000, function(){
   console.log('listening on *:3000');
