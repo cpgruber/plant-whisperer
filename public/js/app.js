6b82d172d9b414e255288e62f1e50ef9070f47fb
Chase Gruber <chasegruber@gmail.com>
2016-01-11 10:48:13 -0500

    transition frontend to angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-  Plant.fetch().then(function(plants){
-    plants.forEach(function(plant){
-      var plantView = new PlantView(plant);
-      plantView.render();
-    })
-    new newPlantView();
-  })
-});
+
