7957060a6657ee86049c3fbcda5ee3c2355af65a
Chase Gruber <chasegruber@gmail.com>
2016-01-12 11:24:53 -0500

    finally fix the date input issue

--- public/js/plants/show.controller.js
+++ public/js/plants/show.controller.js
@@ -10,6 +10,8 @@
   ]);
 
   function PlantShowControllerFunction(PlantFactory, $stateParams){
-    this.plant = PlantFactory.get({id: $stateParams.id});
+    this.plant = PlantFactory.get({id: $stateParams.id}, function(plant){
+      plant.last_water = new Date(plant.last_water)
+    });
   }
 }());
