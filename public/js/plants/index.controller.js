a476b7aad8d0980dc875c60866038969a409c848
Chase Gruber <chasegruber@gmail.com>
2016-01-13 13:14:51 -0500

    index style done

--- public/js/plants/index.controller.js
+++ public/js/plants/index.controller.js
@@ -9,7 +9,14 @@
   ]);
 
   function PlantIndexControllerFunction(PlantFactory){
-    this.plants = PlantFactory.query();
+    this.plants = PlantFactory.query(function(plants){
+      console.log("this is in factory query")
+      console.log(plants.length)
+      for (var i=0;i<plants.length;i++){
+        plants[i].last_water = new Date(plants[i].last_water);
+        plants[i].next_water = new Date(plants[i].next_water)
+      }
+    });
     this.newPlant = new PlantFactory();
   }
 }());
--- public/js/plants/show.controller.js
+++ public/js/plants/show.controller.js
@@ -12,6 +12,7 @@
   function PlantShowControllerFunction(PlantFactory, $stateParams){
     this.plant = PlantFactory.get({id: $stateParams.id}, function(plant){
       plant.last_water = new Date(plant.last_water)
+      plant.next_water = new Date(plant.next_water)
     });
   }
 }());
