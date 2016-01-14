122e7ff8fb8f61c00d45a10d7c3f78cbcccb5cbf
Chase Gruber <chasegruber@gmail.com>
2016-01-14 14:44:51 -0500

    stepwise tweet construction and post

--- db/bot.js
+++ db/bot.js
@@ -14,54 +14,86 @@ var Bot = new Twit({
 });
 
 //first check for plants that need reminders
-//then tweet reminders
-//then check latest tweets for each plant where response is false
-//then check if these tweets have responses from their owner
-//if yes, reset interval
-//if no, set timer for next reminder
-
-function post (plant,content) {
-  Bot.post('statuses/update', { status: content }, function(err, data, response) {
-    if (err){console.log(err)}
-    // console.log(data);
-    console.log("tweeted!")
-    var tweet = new Tweet({
-      createdAt: new Date(),
-      tweetID: data.id_str,
-      response: false,
-      need: "water"
-    });
-    plant.tweets.push(tweet);
-    plant.save(function(err){
-      if (err){
-        console.log(err)
-      }
-    })
-  })
-}
-
-function reply (reply_id) {
-  Bot.post('statuses/update', { status: "Thanks!", in_reply_to_status_id: reply_id }, function(err, data, response) {
-    console.log("tweeted reply!")
-  })
-}
-
 var now = moment();
 Plant.find({'next_water':{
   "$lte":now.toDate()
 }}, function (err, docs){
   docs.forEach(function(doc){
-    var content = "Hey @"+doc.owner+"! Water your "+doc.type+"! 🌱";
-    console.log(content);
-    post(doc,content);
+    //then tweet reminder
+    postReminder(doc);
   })
 })
 
-function getWeather(zip){
-  var w = new Weather(env.wunderground);
-  return new Promise(function(resolve,reject){
-    w.conditions().request(zip, function(err, response){
-      resolve(response.current_observation.temp_f);
-    })
+function makeTweet(tweetData){
+  return new Tweet({
+    createdAt: new Date(),
+    tweetID: tweetData.id_str,
+    response: false,
+    need: "water"
   })
 }
+
+//then tweet reminders
+function postReminder(plantDoc){
+  var content = "Hey @"+plantDoc.owner+"! Water your "+plantDoc.type+"! 💧🌱"
+  Bot.post('statuses/update', {status: content}, function (err,data,response){
+    if (err) return err;
+    var tweet = makeTweet(data);
+    plantDoc.tweets.push(tweet);
+    plantDoc.save(function(err){
+      if (err) console.log(err);
+    })
+
+  })
+}
+
+//then check latest tweets for each plant where response is false
+//then check if these tweets have responses from their owner
+//if yes, reset interval
+//if no, set timer for next reminder
+
+// function post (plant,content) {
+//   Bot.post('statuses/update', { status: content }, function(err, data, response) {
+//     if (err){console.log(err)}
+//     // console.log(data);
+//     console.log("tweeted!")
+//     var tweet = new Tweet({
+//       createdAt: new Date(),
+//       tweetID: data.id_str,
+//       response: false,
+//       need: "water"
+//     });
+//     plant.tweets.push(tweet);
+//     plant.save(function(err){
+//       if (err){
+//         console.log(err)
+//       }
+//     })
+//   })
+// }
+
+// function reply (reply_id) {
+//   Bot.post('statuses/update', { status: "Thanks!", in_reply_to_status_id: reply_id }, function(err, data, response) {
+//     console.log("tweeted reply!")
+//   })
+// }
+
+// var now = moment();
+// Plant.find({'next_water':{
+//   "$lte":now.toDate()
+// }}, function (err, docs){
+//   docs.forEach(function(doc){
+//     var content = "Hey @"+doc.owner+"! Water your "+doc.type+"! 🌱";
+//     console.log(content);
+//     post(doc,content);
+//   })
+// })
+
+// function getWeather(zip){
+//   var w = new Weather(env.wunderground);
+//   return new Promise(function(resolve,reject){
+//     w.conditions().request(zip, function(err, response){
+//       resolve(response.current_observation.temp_f);
+//     })
+//   })
+// }
