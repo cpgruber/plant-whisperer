cab932b57d339b136162ac7f3df9c8cbd54db3cf
Chase Gruber <chasegruber@gmail.com>
2016-01-14 16:56:19 -0500

    comments out most, simple bot on interval with no response

--- db/bot.js
+++ db/bot.js
@@ -4,6 +4,7 @@ var Plant = require("../models/plant")
 var Tweet = require("../models/tweet")
 var Twit = require('twit');
 var moment = require("moment");
+// var Functions = require("./botMethods");
 var Weather = require('wundergroundnode');
 
 var Bot = new Twit({
@@ -12,6 +13,16 @@ var Bot = new Twit({
   access_token: env.accessToken,
   access_token_secret: env.tokenSecret
 });
+
+var now = moment();
+Plant.find({'next_water':{
+  "$lte":now.toDate()
+}}, function (err, plants){
+  plants.forEach(function(plant){
+    postReminder(plant);
+    resetInterval(plant);
+  })
+})
 
 //first check for plants that need reminders
 function remind(){
@@ -41,38 +52,85 @@ function makeTweet(tweetData){
 function postReminder(plantDoc){
   var content = "Hey @"+plantDoc.owner+"! Water your "+plantDoc.type+", please! 💧🌱"
   Bot.post('statuses/update', {status: content}, function (err,data,response){
-    if (err) return err;
-    var tweet = makeTweet(data);
-    plantDoc.tweets.push(tweet);
-    plantDoc.save(function(err){
-      console.log("tweet posted!")
-      if (err) console.log(err);
-    })
+    if (!err){
+      var tweet = makeTweet(data);
+      plantDoc.tweets.push(tweet);
+      plantDoc.save(function(err){
+        console.log("tweet posted!")
+        if (err) console.log(err);
+      })
+    }else{
+      console.log("tweet already posted")
+    }
   })
 }
 
-function checkResponse(){
-  Plant.find({},function(err,plants){
-    plants.forEach(function(plant){
-      var tweets = plant.tweets.sort(function(b,a){return a.createdAt - b.createdAt});
-      var lastTweet = tweets[0];
-      // console.log(lastTweet)
-      if (lastTweet){
-        Bot.get('search/tweets',{q:"@Your_Plants",in_reply_to_status_id:lastTweet.tweetID,}, function(err, data, response) {
-          var text = data.statuses[0].text;
-          var tweetUserId = data.statuses[0].user.id_str;
-          // console.log(text.indexOf("💧"))
-          if (text.indexOf("💧") > -1 && tweetUserId == lastTweet.recipientID){
-            console.log("thank you! resetting your interval")
-          }
-        })
-      }
-    })
+function resetInterval(plantDoc){
+  plantDoc.last_water =  moment();
+  plantDoc.next_water= moment().add(plantDoc.frequency,plantDoc.interval)
+  plantDoc.save(function(err){
+    if (!err){
+      console.log("plant saved")
+    }
   })
 }
+
+// function searchReplies(tweet){
+//   return new Promise(function(resolve,reject){
+//     Bot.get('search/tweets',{q:"@Your_Plants",in_reply_to_status_id:tweet.tweetID},function(err,data,response){
+//       if (data.statuses.length > 0){
+//         var text = data.statuses[0].text;
+//         var tweetUserId = data.statuses[0].id_str;
+//         if (text.indexOf("💧") > -1 && tweetUserId == tweet.recipientID){
+//           console.log("thanks! resetting interval")
+//           resolve(true)
+//         }
+//       }
+//     })
+//   })
+// }
+
+// function checkResponse(){
+//   Plant.find({},function(err,plants){
+//     for (var i=0;i<plants.length;i++){
+//       var plant = plants[i]
+//       if (plant.tweets.length == 0){
+//         console.log("no tweets");
+//         continue;
+//       }
+//       var tweets = plant.tweets.sort(function(b,a){return a.createdAt - b.createdAt});
+//       var lastTweet = tweets[0];
+//       searchReplies(lastTweet).then(function(response){
+//         console.log("here is the response")
+//         console.log(response)
+//       })
+//       if (lastTweet){
+//         Bot.get('search/tweets',
+//         {q:"@Your_Plants",in_reply_to_status_id:lastTweet.tweetID},
+//         function(err, data, response) {
+//           if (data.statuses.length > 0){
+//             var text = data.statuses[0].text;
+//             console.log(text)
+//             var tweetUserId = data.statuses[0].user.id_str;
+//             if (text.indexOf("💧") > -1 && tweetUserId == lastTweet.recipientID){
+//               console.log("thank you! resetting your interval")
+//               lastTweet.response = true;
+//               plant.save(function(err){
+//                 if (err){console.log(err)}
+//               })
+//             }
+//           }else{
+//             console.log("no responses yet")
+//           }
+//         })
+//       }
+//     }
+//     })
+//   })
+// }
 
 // remind();
-checkResponse();
+// checkResponse();
 
 //then check latest tweets for each plant where response is false
 //then check if these tweets have responses from their owner
